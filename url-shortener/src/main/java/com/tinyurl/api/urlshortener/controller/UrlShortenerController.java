60471ff6766653e3b205911ece90b61c7a5efa76
adityasawant <adityasawant.294@gmail.com>
2024-11-06 10:54:02 -0500

    refactor

--- url-shortener/src/main/java/com/tinyurl/api/urlshortener/controller/UrlShortenerController.java
+++ url-shortener/src/main/java/com/tinyurl/api/urlshortener/controller/UrlShortenerController.java
@@ -1,6 +1,5 @@
 package com.tinyurl.api.urlshortener.controller;
 
-import com.tinyurl.api.urlshortener.exception.UrlException;
 import com.tinyurl.api.urlshortener.model.UrlDTO;
 import com.tinyurl.api.urlshortener.model.UrlRequestModel;
 import com.tinyurl.api.urlshortener.service.UrlShortenerService;
@@ -8,7 +7,6 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -23,23 +21,7 @@ public class UrlShortenerController {
     public UrlShortenerController(UrlShortenerService urlShortenerService) {
         this.urlShortenerService = urlShortenerService;
     }
-    //take longUrl as string input
-    @PostMapping(
-            value = "/v1/shorten",
-            consumes = "application/json",
-            produces = "application/json"
-    )
-    public ResponseEntity<String> shortenUrlMongo(@Valid @RequestBody UrlRequestModel urlRequestModel) {
 
-        UrlDTO urlDTO =  urlShortenerService.shortenUrl(urlRequestModel.getLongUrl());
-        HttpStatus status = null;
-        if (!urlDTO.isNew()) {
-            status = HttpStatus.CONFLICT;
-        } else {
-            status = HttpStatus.CREATED;
-        }
-        return new ResponseEntity<>(urlDTO.getShortUrl(), status);
-    }
 
     @PostMapping(
             value = "/v2/shorten",
@@ -48,7 +30,7 @@ public class UrlShortenerController {
     )
     public ResponseEntity<UrlDTO> shortenUrlRedis(@Valid @RequestBody UrlRequestModel urlRequestModel) {
 
-        UrlDTO urlDTO =  urlShortenerService.shortenUrlV2(urlRequestModel.getLongUrl());
+        UrlDTO urlDTO =  urlShortenerService.shortenUrl(urlRequestModel.getLongUrl());
         HttpStatus status;
         if (!urlDTO.isNew()) {
             status = HttpStatus.OK;
@@ -59,31 +41,12 @@ public class UrlShortenerController {
     }
 
     @GetMapping(
-            value = "/v1/{shorturl}"
-    )
-    public ResponseEntity<?> redirectToLongUrlMongo(@PathVariable(name="shorturl") String shortUrl) {
-
-        // TODO: implement actual redirection logic
-        String longUrl = urlShortenerService.getLongUrl(shortUrl);
-
-        if (longUrl != null) {
-            HttpHeaders headers = new HttpHeaders();
-            headers.add("Location", longUrl);
-
-            return new ResponseEntity<>(headers, HttpStatus.FOUND);
-        } else {
-            return new ResponseEntity<>("URL not found", HttpStatus.NOT_FOUND);
-        }
-
-    }
-
-    @GetMapping(
             value = "/v2/{shortUrl}"
     )
     public ResponseEntity<?> redirectToLongUrlRedis(@PathVariable(name="shortUrl") String shortUrl) {
 
         // TODO: implement actual redirection logic
-        String longUrl = urlShortenerService.getLongUrlV2(shortUrl);
+        String longUrl = urlShortenerService.getLongUrl(shortUrl);
 
         if (longUrl != null) {
             HttpHeaders headers = new HttpHeaders();
--- url-shortener/src/main/java/com/tinyurl/api/urlshortener/service/UrlShortenerServiceImpl.java
+++ url-shortener/src/main/java/com/tinyurl/api/urlshortener/service/UrlShortenerServiceImpl.java
@@ -1,11 +1,6 @@
 package com.tinyurl.api.urlshortener.service;
 
-import com.tinyurl.api.urlshortener.entity.ShortUrl;
-import com.tinyurl.api.urlshortener.entity.TinyUrl;
-import com.tinyurl.api.urlshortener.exception.UrlException;
 import com.tinyurl.api.urlshortener.model.UrlDTO;
-import com.tinyurl.api.urlshortener.repository.ShortUrlRepository;
-import com.tinyurl.api.urlshortener.repository.TinyUrlRepository;
 import com.tinyurl.api.urlshortener.util.UrlUtil;
 import org.modelmapper.ModelMapper;
 import org.slf4j.Logger;
@@ -13,9 +8,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.hash.HashMapper;
-import org.springframework.data.redis.hash.ObjectHashMapper;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -24,10 +16,7 @@ public class UrlShortenerServiceImpl implements UrlShortenerService{
 
     private static final Logger log = LoggerFactory.getLogger(UrlShortenerServiceImpl.class);
     private final UrlUtil urlUtil;
-    private final ShortUrlRepository shortUrlRepository;
-    private final ModelMapper modelMapper;
     private final RedisTemplate<String, String> redisTemplate;
-//    private TinyUrlRepository tinyUrlRepository;
 
     @Value("${spring.redis.shorten_prefix}")
     private String longToShortPrefix;
@@ -36,56 +25,14 @@ public class UrlShortenerServiceImpl implements UrlShortenerService{
     private String redirectPrefix;
 
     @Autowired
-    public UrlShortenerServiceImpl(UrlUtil urlUtil, ShortUrlRepository shortUrlRepository, ModelMapper modelMapper, RedisTemplate<String, String> redisTemplate) {
+    public UrlShortenerServiceImpl(UrlUtil urlUtil, ModelMapper modelMapper, RedisTemplate<String, String> redisTemplate) {
         this.urlUtil = urlUtil;
-        this.shortUrlRepository = shortUrlRepository;
-        this.modelMapper = modelMapper;
         this.redisTemplate = redisTemplate;
     }
 
-    @Override
     public UrlDTO shortenUrl(String longUrl) {
-
-        log.info("Shortening URL: {}", longUrl);
-        UrlDTO urlDTO = null;
-
-        ShortUrl shortUrlEntity = shortUrlRepository.findByLongUrl(longUrl);
-
-        if (shortUrlEntity == null) {
-            urlDTO = urlUtil.generateShortUrl(longUrl);
-
-            shortUrlEntity = modelMapper.map(urlDTO, ShortUrl.class);
-
-            try {
-                shortUrlRepository.save(shortUrlEntity);
-            } catch (Exception e) {
-                throw new UrlException("Database error while saving short URL.", HttpStatus.INTERNAL_SERVER_ERROR);
-            }
-
-        } else {
-            urlDTO = new UrlDTO(shortUrlEntity.getKey(), shortUrlEntity.getLongUrl(), shortUrlEntity.getShortUrl(), false);
-        }
-
-        return urlDTO;
-    }
-
-
-    @Override
-    public String getLongUrl(String shortUrl) {
-        log.info("Getting long URL for short URL: {}", shortUrl);
-        try {
-            ShortUrl shortUrlEntity = shortUrlRepository.findByKey(shortUrl);
-
-            return shortUrlEntity != null ? shortUrlEntity.getLongUrl() : null;
-        } catch (Exception e) {
-            throw new UrlException("Database error while fetching long URL.", HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
-
-
-    public UrlDTO shortenUrlV2(String longUrl) {
         log.info("Shortening URL - Version 2: {}", longUrl);
-        UrlDTO urlDTO = null;
+        UrlDTO urlDTO;
 
         String shortUrl = redisTemplate.opsForValue().get(longToShortPrefix + longUrl);
         if (shortUrl == null) {
@@ -103,7 +50,7 @@ public class UrlShortenerServiceImpl implements UrlShortenerService{
     }
 
     @Override
-    public String getLongUrlV2(String shortUrl) {
+    public String getLongUrl(String shortUrl) {
         log.info("Getting long URL for short URL: {}", shortUrl);
 
         return redisTemplate.opsForValue().get(redirectPrefix + shortUrl);
